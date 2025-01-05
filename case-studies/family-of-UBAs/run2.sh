2c87cf36cc490881d91ac138454145f7e01957ee
Qiyi Tang <qiyitang@Qiyis-Air.home>
2025-01-05 17:08:45 +0000

    add scripts to print out results

--- case-studies/family-of-UBAs/run2.sh
+++ case-studies/family-of-UBAs/run2.sh
@@ -1,16 +1,19 @@
 #!/bin/bash 
 
-for i in $(seq 3 10);
+for i in $(seq 9 9);
 do 
 
   { echo -e "Generating uba-$i \n"; } > results/"uba-$i.txt"
   python3 uba-gen.py $i
+
   #UBA model checking !use timeout on LINUX 
   gtimeout 30m ../../prism/bin/prism -explicit -ltluba -ubaverbosity 1 random2.pm -pf 'P=?[ HOA: {"'"UBAs/uba-$i.hoa"'", "sigma" <- s=0, "pi" <- s=1, "hash" <- s=2, "dollar" <- s=3}]' >> results/"uba-$i.txt"
 
-  #timing the transformation
+  #timing the transformations
   { echo -e "Transforming to GFG-NCA gfg-$i \n"; } > results/"gfg-$i.txt"
+  TIMEFORMAT=%R
   { time ../../../ltl2pba/uba2pba -a "UBAs/uba-$i.hoa" -o "GFGs/gfg-$i.hoa"; } >> results/"gfg-$i.txt" 2>&1
+
   { echo -e "\n"; } >> results/"gfg-$i.txt"
   #GFG model checking !use timeout on LINUX 
   gtimeout 30m ../../prism/bin/prism -explicit -gfgmc -ubaverbosity 1 random2.pm -pf 'P=?[ HOA: {"'"GFGs/gfg-$i.hoa"'", "sigma_0" <- s=0, "pi_0" <- s=1, "hash_0" <- s=2, "dollar_0" <- s=3 }]' >> results/"gfg-$i.txt"
