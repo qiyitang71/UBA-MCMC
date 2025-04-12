6876378fbff5612c46aed813d98e607e626651c4
qiyitang71 <qiyi.tang71@gmail.com>
2025-04-12 17:21:27 +0100

    Delete case-studies directory

--- case-studies/family-of-UBAs/gen-labels-lmc.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-labels = ["sigma", "pi", "hash", "dollar"]
-
-# Open the file in write mode
-with open("labels.txt", "w") as file:
-    for i in range(500):  # Two sets: _0 and _1
-        for j, label in enumerate(labels):
-            file.write(f'label "{label}_{i}" = s={i * len(labels) + j};\n')
